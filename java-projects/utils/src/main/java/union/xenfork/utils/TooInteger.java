4ce391e7fba8755428085b77321f864880850c32
baka4n <qq474899581@gmail.com>
2023-07-03 12:03:39 +0800

    忘记没有考虑正负问题准备对其进行重做

--- java-projects/utils/src/main/java/union/xenfork/utils/TooInteger.java
+++ java-projects/utils/src/main/java/union/xenfork/utils/TooInteger.java
@@ -2,6 +2,7 @@ package union.xenfork.utils;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -9,73 +10,71 @@ import java.util.Set;
 public class TooInteger implements Serializable {
     @Serial
     private static final long serialVersionUID = -7343907856428713228L;
-    private final Map<Integer, Integer> integerMap = new HashMap<>();//三位一存
-
-    public TooInteger(String integer) {
-        if (integer.length() < 4) {
-            integerMap.put(0, Integer.parseInt(integer));
-        } else {
-            StringBuilder sb = new StringBuilder();
-            for (int i = integer.length() - 1; i >= 0; i--) {
-                sb.insert(0, integer.charAt(i));
-                if (sb.length() == 3) {
-                    integerMap.put(integerMap.size(), Integer.parseInt(sb.toString()));
-                    sb = new StringBuilder();
-                }
-            }
-            if (!sb.isEmpty()) {
-                integerMap.put(integerMap.size(), Integer.parseInt(sb.toString()));
-            }
-        }
-    }
-
-    protected TooInteger(TooInteger integer) {
-        this.integerMap.putAll(integer.integerMap);
-    }
-
-    public TooInteger copy() {
-        return new TooInteger(this);
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < integerMap.size(); i++) {
-            if (i == integerMap.size() - 1) {
-                sb.insert(0, integerMap.get(i));
-            } else {
-                sb.insert(0, "%03d".formatted(integerMap.get(i)));
-            }
-        }
-        return sb.toString();
-    }
-
-    public TooInteger add(TooInteger integer) {
-        TooInteger copy = integer.copy();
-        Set<Integer> integers = copy.integerMap.keySet();
-        int mod = 0;
-        int integer_ = 0;
-        for (int i = 0; i < integers.size(); i++) {
-            int primitive;
-            integer_ = i;
-            primitive = this.integerMap.getOrDefault(i, 0);//原数
-            Integer addend = copy.integerMap.get(i);//加数
-            int i1 = primitive + addend + mod;
-            mod = i1 / 1000;
-            System.out.println(mod);
-            this.integerMap.put(i, i1 % 1000);
-        }
-        while (mod != 0) {
-            integer_++;
-            if (this.integerMap.containsKey(integer_)) {
-                int i1 = mod + this.integerMap.get(integer_);
-                mod = i1 / 1000;
-                this.integerMap.put(integer_, i1 % 1000);
-            } else {
-                this.integerMap.put(integer_, mod);
-                mod = 0;
-            }
-        }
-        return this;
-    }
+//    private final Map<Integer, Integer> integerMap = new HashMap<>();//三位一存
+//    public TooInteger(String integer) {
+//        if (integer.length() < 4) {
+//            integerMap.put(0, Integer.parseInt(integer));
+//        } else {
+//            StringBuilder sb = new StringBuilder();
+//            for (int i = integer.length() - 1; i >= 0; i--) {
+//                sb.insert(0, integer.charAt(i));
+//                if (sb.length() == 3) {
+//                    integerMap.put(integerMap.size(), Integer.parseInt(sb.toString()));
+//                    sb = new StringBuilder();
+//                }
+//            }
+//            if (!sb.isEmpty()) {
+//                integerMap.put(integerMap.size(), Integer.parseInt(sb.toString()));
+//            }
+//        }
+//    }
+//
+//    protected TooInteger(TooInteger integer) {
+//        this.integerMap.putAll(integer.integerMap);
+//    }
+//
+//    public TooInteger copy() {
+//        return new TooInteger(this);
+//    }
+//
+//    @Override
+//    public String toString() {
+//        StringBuilder sb = new StringBuilder();
+//        for (int i = 0; i < integerMap.size(); i++) {
+//            if (i == integerMap.size() - 1) {
+//                sb.insert(0, integerMap.get(i));
+//            } else {
+//                sb.insert(0, "%03d".formatted(integerMap.get(i)));
+//            }
+//        }
+//        return sb.toString();
+//    }
+//
+//    public TooInteger add(TooInteger integer) {
+//        TooInteger copy = integer.copy();
+//        int mod = 0;
+//        int integer_ = 0;
+//        for (int i = 0; i < copy.integerMap.size(); i++) {
+//            int primitive;
+//            integer_ = i;
+//            primitive = this.integerMap.getOrDefault(i, 0);//原数
+//            Integer addend = copy.integerMap.get(i);//加数
+//            int i1 = primitive + addend + mod;
+//            mod = i1 / 1000;
+//            this.integerMap.put(i, i1 % 1000);
+//        }
+//        while (mod != 0) {
+//            integer_++;
+//            int i1 = mod + this.integerMap.getOrDefault(integer_, 0);
+//            mod = i1 / 1000;
+//            this.integerMap.put(integer_, i1 % 1000);
+//        }
+//        return this;
+//    }
+//
+//    public TooInteger reduce(TooInteger integer) {
+//        TooInteger copy = integer.copy();
+//
+//        return this;
+//    }
 }
--- java-projects/utils/src/test/java/TestTooInteger.java
+++ java-projects/utils/src/test/java/TestTooInteger.java
@@ -4,14 +4,14 @@ import union.xenfork.utils.TooInteger;
 public class TestTooInteger {
     @Test
     public void test() {
-        TooInteger tooInteger = new TooInteger("1000000000");
-        System.out.println(tooInteger);
+//        TooInteger tooInteger = new TooInteger("1000000000");
+//        System.out.println(tooInteger);
     }
     @Test
     public void addTest() {
-        TooInteger tooInteger = new TooInteger("100000000");
-//        tooInteger.add(tooInteger);
-        tooInteger.add(new TooInteger("900000000"));
-        System.out.println(tooInteger);
+//        TooInteger tooInteger = new TooInteger("100000000");
+////        tooInteger.add(tooInteger);
+//        tooInteger.add(new TooInteger("900000000"));
+//        System.out.println(tooInteger);
     }
 }
