d547217a68198a4182ab5255c1862390a2a65ac2
baka4n <qq474899581@gmail.com>
2023-07-03 17:34:35 +0800

    先上传

--- java-projects/utils/src/main/java/union/xenfork/utils/TooInteger.java
+++ java-projects/utils/src/main/java/union/xenfork/utils/TooInteger.java
@@ -1,80 +1,50 @@
 package union.xenfork.utils;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.math.BigInteger;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 public class TooInteger implements Serializable {
+
     @Serial
     private static final long serialVersionUID = -7343907856428713228L;
-//    private final Map<Integer, Integer> integerMap = new HashMap<>();//三位一存
-//    public TooInteger(String integer) {
-//        if (integer.length() < 4) {
-//            integerMap.put(0, Integer.parseInt(integer));
-//        } else {
-//            StringBuilder sb = new StringBuilder();
-//            for (int i = integer.length() - 1; i >= 0; i--) {
-//                sb.insert(0, integer.charAt(i));
-//                if (sb.length() == 3) {
-//                    integerMap.put(integerMap.size(), Integer.parseInt(sb.toString()));
-//                    sb = new StringBuilder();
-//                }
-//            }
-//            if (!sb.isEmpty()) {
-//                integerMap.put(integerMap.size(), Integer.parseInt(sb.toString()));
-//            }
-//        }
-//    }
-//
-//    protected TooInteger(TooInteger integer) {
-//        this.integerMap.putAll(integer.integerMap);
-//    }
-//
-//    public TooInteger copy() {
-//        return new TooInteger(this);
-//    }
-//
-//    @Override
-//    public String toString() {
-//        StringBuilder sb = new StringBuilder();
-//        for (int i = 0; i < integerMap.size(); i++) {
-//            if (i == integerMap.size() - 1) {
-//                sb.insert(0, integerMap.get(i));
-//            } else {
-//                sb.insert(0, "%03d".formatted(integerMap.get(i)));
-//            }
-//        }
-//        return sb.toString();
-//    }
-//
-//    public TooInteger add(TooInteger integer) {
-//        TooInteger copy = integer.copy();
-//        int mod = 0;
-//        int integer_ = 0;
-//        for (int i = 0; i < copy.integerMap.size(); i++) {
-//            int primitive;
-//            integer_ = i;
-//            primitive = this.integerMap.getOrDefault(i, 0);//原数
-//            Integer addend = copy.integerMap.get(i);//加数
-//            int i1 = primitive + addend + mod;
-//            mod = i1 / 1000;
-//            this.integerMap.put(i, i1 % 1000);
-//        }
-//        while (mod != 0) {
-//            integer_++;
-//            int i1 = mod + this.integerMap.getOrDefault(integer_, 0);
-//            mod = i1 / 1000;
-//            this.integerMap.put(integer_, i1 % 1000);
-//        }
-//        return this;
-//    }
-//
-//    public TooInteger reduce(TooInteger integer) {
-//        TooInteger copy = integer.copy();
-//
-//        return this;
-//    }
+    public Integer cbr, dbr, nbr, bbr, ybr, zbr, ebr, pbr, tbr, gbr, mbr, kbr, br, bitr;//寄存器
+    //用于寄存计算冗余量
+    public int cb = 0, db= 0, nb= 0, bb= 0, yb= 0, zb= 0, eb= 0, pb= 0, tb= 0, gb= 0, mb= 0, kb= 0, b= 0, bit= 0;
+
+
+    // 存储单位算数法
+
+    public TooInteger(@NotNull Integer integer) {
+        int mod = 0, level = 0;
+        if (integer >= 8) {
+            bs[0] = integer % 8;
+            
+            save();
+        } else {
+            bs[0] = integer;
+            save();
+        }
+    }
+    public int[] bs = new int[] {
+            bit, b, kb, mb, gb, tb, pb, eb, zb, yb, bb, nb, db, cb
+    };
+    public void save() {
+        bit = bs[0];
+        b = bs[1];
+        kb = bs[2];
+        mb = bs[3];
+        gb = bs[4];
+        tb = bs[5];
+        pb = bs[6];
+        eb = bs[7];
+        zb = bs[8];
+        yb = bs[9];
+        bb = bs[10];
+        nb = bs[11];
+        db = bs[12];
+        cb = bs[13];
+    }
 }
--- java-projects/utils/src/test/java/TestTooInteger.java
+++ java-projects/utils/src/test/java/TestTooInteger.java
@@ -1,17 +1,19 @@
 import org.junit.jupiter.api.Test;
 import union.xenfork.utils.TooInteger;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 public class TestTooInteger {
     @Test
     public void test() {
-//        TooInteger tooInteger = new TooInteger("1000000000");
-//        System.out.println(tooInteger);
-    }
-    @Test
-    public void addTest() {
-//        TooInteger tooInteger = new TooInteger("100000000");
-////        tooInteger.add(tooInteger);
-//        tooInteger.add(new TooInteger("900000000"));
-//        System.out.println(tooInteger);
+        StringBuilder a = new StringBuilder("9.");
+        String repeat = "999999999999999".repeat(10000);
+        a.append(repeat);
+        a.append("E+393000");
+        BigDecimal bigDecimal = new BigDecimal(a.toString());
+        BigInteger bigInteger = bigDecimal.toBigInteger();
+        System.out.println(bigInteger.multiply(bigInteger));
+//        System.out.println(bigInteger);
     }
 }
